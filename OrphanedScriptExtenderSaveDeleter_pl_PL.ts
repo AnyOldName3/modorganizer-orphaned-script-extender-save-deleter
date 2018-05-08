f19ddc2365b3768e3f0f5bc80042f14f7884ba99
Yoosk <yooskacz@gmail.com>
2018-05-08 19:22:51 +0000

    more polish files.

---
+++ OrphanedScriptExtenderSaveDeleter_pl_PL.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>OrphanedScriptExtenderSaveDeleter</name>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="45"/>
+        <source>Deletes script extender saves which don&apos;t have a corresponding base game save.</source>
+        <translation>Usuwa zapisy Script Extender które nie mają odpowiadającego mu bazowego pliku.</translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="42"/>
+        <source>Orphaned Script Extender Save Deleter</source>
+        <translation>Orphaned Script Extender Save Deleter</translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="55"/>
+        <source>Before starting deletion...</source>
+        <translation>Przed rozpoczęciem usuwania...</translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="55"/>
+        <source>Please double check that you want your orphaned script extender saves deleted. If you proceed, you won&apos;t be able to get them back, even if you find you&apos;ve copied the corresponding base game save somewhere else so still have a copy.</source>
+        <translation>Proszę podwójnie sprawdzić czy chcesz usunąc osierocone pliki Script Extender. Jeżeli kontynuujesz, nie będziesz mógł ich odzyskać spowrotem, nawet jeśli znajdziesz że skopiowałeś odopowiedni plik bazowy gdzieś indziej.</translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="71"/>
+        <source>No orphaned script extender saves found</source>
+        <translation>Nie znaleziono osieroconych zapisów Script Extender</translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="71"/>
+        <source>No orphaned script extender co-saves were found, so none were removed.</source>
+        <translation>Nie znaleziono osieroconych współ-zapisów Script Extender, więc nic nie zostało usunięte.</translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="73"/>
+        <source>Orphaned script extender saves removed</source>
+        <translation>Osierocone zapisy Script Extender zostały usunięte</translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="73"/>
+        <source>{0} orphaned script extender co-save(s) removed successfully.</source>
+        <translation>{0} osierocony(ch) zapisów Script Extender zostało usuniętych poprawnie.</translation>
+    </message>
+</context>
+</TS>
